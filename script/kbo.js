3235b870d3ea3cfd3cc2dbc31a56c2b10de25dae
God_Lights <backup.xtapu3@gmail.com>
2024-11-09 19:07:46 +0900

    수정

---
+++ script/kbo.js
@@ -0,0 +1,49 @@
+const kboTeam = {
+    "키움":"WO",
+    "LG":"LG",
+    "KT":"KT",
+    "KIA":"HT",
+    "두산":"OB",
+    "NC":"NC",
+    "삼성":"SS",
+    "롯데":"LT",
+    "한화":"HH",
+    "SSG":"SK"
+}
+
+
+const kboScrollData = {
+    "leId":1,
+    "srId":0,
+    "seasonId":2024,
+    "gameId":"20240930WOSK0"
+};
+function fop(season, month, day, hometeam, awayteam, le, sr) {
+    var m = "";
+    var d = "";
+    kboScrollData["leId"] = Number(le);
+    kboScrollData["srId"] = Number(sr);
+    kboScrollData["seasonId"] = Number(season);
+    if(Number(month) < 10) {
+        m = `0${month}`;
+    } else {
+        m = month;
+    }
+    if(Number(day) < 10) {
+        d = `0${day}`;
+    } else {
+        d = day;
+    }
+    kboScrollData["gameId"] = season+m+d+hometeam+awayteam+"0";
+    return kboScrollData;
+}
+
+function selectTeam() {
+    var sel = document.getElementById('team');
+    return sel;
+}
+function fo() {
+    var foo = document.getElementById('fo');
+    foo.innerHTML = selectTeam().value;
+    requestAnimationFrame(fo);
+}
