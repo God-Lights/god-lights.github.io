2c4fe9f3c4eb1a74d2d90d802ed819b2a90e24de
God_Lights <backup.xtapu3@gmail.com>
2025-01-04 23:35:13 +0900

    수정

--- js/githubButton/githubButton.js
+++ js/githubButton/githubButton.js
@@ -1,23 +1,52 @@
 import "https://kit.fontawesome.com/1a245537f8.js";
 
-hi();
-
-function hi() {
-    var link = document.createElement('link');
-    // link의 속성 설정
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'style.css';
-    // link를 문서 head에 추가
-    document.getElementsByTagName('head')[0].appendChild(link);
-  let h = document.querySelectorAll('gitprof');
-  h.forEach(function(element) {
-    let usenan = element.attributes["username"].value;
-    element.onclick = function() {
-      window.location.href = `https://github.com/${usenan}`;
-      console.log(usenan);
-    }
-  });
-  console.log(h.length);
-  return h;
+function loadGithubButtons() {
+    let h = document.querySelectorAll('gitprof');
+    h.forEach(function(element) {
+        let usenan = element.attributes["username"].value;
+        element.style.fontFamily = '"Font Awesome 6 Brands", sans-serif';
+        if(element.attributes["size"] != undefined) {
+            element.style.fontSize = element.attributes["size"].value;
+        }  else {
+            element.style.fontSize = "25px";
+        }
+        element.style.cursor = "pointer";
+        element.innerHTML = "\uf092";
+        document.onselectstart = function() {
+            return false;
+        }
+        element.onclick = function() {
+            window.location.href = "https://github.com/"+usenan;
+        }
+    });
+    console.log(`${h.length} Github Buttons are created.`);
 }
+
+function loadYoutubeButtons() {
+    let h = document.querySelectorAll('ytprof');
+    h.forEach(function(element) {
+        let usenan = element.attributes["userid"].value;
+        element.style.fontFamily = '"Font Awesome 6 Brands", sans-serif';
+        if(element.attributes["size"] != undefined) {
+            element.style.fontSize = element.attributes["size"].value;
+        } else {
+            element.style.fontSize = "25px";
+        }
+        element.style.cursor = "pointer";
+        element.innerHTML = "\uf167";
+        document.onselectstart = function() {
+            return false;
+        }
+        element.onclick = function() {
+            window.location.href = "https://youtube.com/c/"+usenan;
+        }
+    });
+    console.log(`${h.length} Youtube Buttons are created.`);
+}
+
+function load() {
+    loadGithubButtons();
+    loadYoutubeButtons();
+}
+
+load();
