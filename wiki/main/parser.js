36eeec715c98d4c10132b50decc40274b363fa92
GodLights <80441168+God-Lights@users.noreply.github.com>
2025-07-01 12:17:48 +0900

    Update parser.js

--- wiki/main/parser.js
+++ wiki/main/parser.js
@@ -61,6 +61,9 @@ function processScript(params) {
       return setDday(params.date);
     case "dualnty":
       return dualCountry(params.c1,params.c2);
+    case "redirect":
+	    window.location.href = params.link || "about:blank"
+	    return "redirect: "+params.link
     default:
       return "[지원되지 않는 타입]";
   }
